a633896b21b126a7879a5e6ea6cf7a820e0b1e41
krysiajanik <a.krystyna.janik@gmail.com>
2020-06-29 18:59:07 +0200

    feat: more robust repo list added

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,14 +1,24 @@
-fetch('https://api.github.com/users/krysiajanik/repos?sort=created&direction=asc')
-.then(resp => resp.json())
-.then(resp => {
+fetch(
+  "https://api.github.com/users/krysiajanik/repos?sort=created&direction=desc"
+)
+  .then((resp) => resp.json())
+  .then((resp) => {
     for (let repo of resp) {
-        const {name, html_url} = repo;
-        const repoList = document.querySelector('.repo-list--js');
-        const listHTML = `<li class="repo-list__item"><a href="${html_url}" title="link do repozytorium ${name} na githubie">${name}</li>`;
-        repoList.innerHTML += listHTML;
+      const { name, html_url } = repo;
+      const repoList = document.querySelector(".repo-list--js");
+      const listHTML = `<li class="repo-list__item">${name} 
+      <ul> 
+      <li><a href="${html_url}" title="link do repozytorium na githubie">repozytorium na githubie</a></li>`;
+      if (repo.has_pages === true) {
+        const ghPages = `<li><a href="https://krysiajanik.github.io/${name}/" title="link do github pages">github pages</a></li>`;
+        repoList.innerHTML += listHTML.concat(ghPages, '</li>');
+      }else {
+        repoList.innerHTML += listHTML.concat('</li>');
+      }
+
+      
     }
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.log(error);
-    
-})
+  });

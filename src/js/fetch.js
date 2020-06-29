7141c39829a142c0c836f56e004e7aeed4ea016f
krysiajanik <a.krystyna.janik@gmail.com>
2020-06-29 18:36:09 +0200

    feat: styling fir repo list done

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -4,7 +4,7 @@ fetch('https://api.github.com/users/krysiajanik/repos?sort=created&direction=asc
     for (let repo of resp) {
         const {name, html_url} = repo;
         const repoList = document.querySelector('.repo-list--js');
-        const listHTML = `<li><a href="${html_url}" title="link do repozytorium na githubie">${name}<li>`;
+        const listHTML = `<li class="repo-list__item"><a href="${html_url}" title="link do repozytorium ${name} na githubie">${name}</li>`;
         repoList.innerHTML += listHTML;
     }
 })
